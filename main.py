b13945cbe4d13a57866c808a90bfc24f4b997faa
Muhammad Abid <abidroid1@gmail.com>
2024-12-31 15:57:45 +0500

    pdf upload

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from dotenv import load_dotenv
+from pypdf import PdfReader
 
-st.title("PDF Chat")
+load_dotenv()
+st.set_page_config(page_title="Ask your PDF")
+st.header("Ask your PDF 😎")
+
+# upload file
+pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+if pdf is not None:
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    st.write(text)
