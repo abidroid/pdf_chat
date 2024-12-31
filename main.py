7b2c0d22bd303a684d2c17c57bbacfb301f9dccd
Muhammad Abid <abidroid1@gmail.com>
2024-12-31 17:07:53 +0500

    chunks done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from pypdf import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 st.set_page_config(page_title="Ask your PDF")
@@ -15,4 +16,12 @@ if pdf is not None:
     for page in pdf_reader.pages:
         text += page.extract_text()
 
-    st.write(text)
+          # split into chunks
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
